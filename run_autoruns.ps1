0010be5a572ddaaa994f2af2b1f7f95343d28c0e
crrim <5191005+crrim@users.noreply.github.com>
2021-05-19 12:56:43 +0100

    Update run_autoruns.ps1

--- run_autoruns.ps1
+++ run_autoruns.ps1
@@ -1,3 +1,2 @@
 #run autoruns
-$user = $args[0]
-& "C:\Users\"+$user+"\AppData\Local\Temp\Session_f\autorunsc64.exe" -accepteula -a * -ct > "C:\Users\"+$user+"\AppData\Local\Temp\Session_f\autoruns.csv"
+& "C:\Users\session_f\autorunsc64.exe" -accepteula -a * -ct > "C:\session_f\autoruns.csv"
